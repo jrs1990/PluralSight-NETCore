9ec40a888c38295d3a39727847fd0c467e153ea1
Jander Reis <jrs_1990@hotmail.com>
2018-09-29 14:52:28 -0300

    commit de finalização do módulo "Using the Entity Framework"

---
+++ OdeToFood/OdeToFood/Services/SqlRestaurantData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OdeToFood.Data;
+using OdeToFood.Model;
+
+namespace OdeToFood.Services
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private OdeToFoodDBContext _context;
+        public SqlRestaurantData(OdeToFoodDBContext context)
+        {
+            _context = context;
+        }
+        public Restaurant Add(Restaurant newRestaurante)
+        {
+            _context.Restaurants.Add(newRestaurante);
+            _context.SaveChanges();
+            return newRestaurante;
+        }
+
+        public Restaurant Get(int id)
+        {
+            return _context.Restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+           return _context.Restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
